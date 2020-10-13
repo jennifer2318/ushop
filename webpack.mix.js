b1354c2ca778b4e4a0559248a509c779057222ac
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2020-10-13 18:50:00 +0300

    update package / webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,33 +14,39 @@ const mix = require('laravel-mix');
 mix.config.babelConfig =
 {
     "plugins": [
-        ["@babel/plugin-proposal-decorators", { "legacy": true },
+        [
+            // "@babel/plugin-proposal-decorators", { "legacy": true },
 
             //Minify
-            "@babel/plugin-transform-minify-booleans",
-            "@babel/plugin-minify-builtins",
-            "@babel/plugin-minify-dead-code-elimination", { "optimizeRawSize": true },
-            "@babel/plugin-minify-mangle-names",
-            "@babel/plugin-minify-numeric-literals",
-            "@babel/plugin-minify-simplify",
-            "@babel/plugin-transform-simplify-comparison-operators",
-            "@babel/plugin-minify-type-constructors",
-            "@babel/plugin-transform-member-expression-literals",
+            // "@babel/plugin-transform-minify-booleans",
+            // "@babel/plugin-minify-builtins",
+            // "@babel/plugin-minify-dead-code-elimination", { "optimizeRawSize": true },
+            // "@babel/plugin-minify-mangle-names",
+            // "@babel/plugin-minify-numeric-literals",
+            // "@babel/plugin-minify-simplify",
+            // "@babel/plugin-transform-simplify-comparison-operators",
+            // "@babel/plugin-minify-type-constructors",
+            // "@babel/plugin-transform-member-expression-literals",
 
             //Experimental
-            "@babel/plugin-proposal-nullish-coalescing-operator",
+            // "@babel/plugin-proposal-nullish-coalescing-operator",
 
             // Other
-            "@babel/plugin-transform-minify-booleans",
-            "minify-builtins",
-            "minify-dead-code-elimination", { "optimizeRawSize": true },
-            "minify-mangle-names",
-            "minify-numeric-literals",
-            "minify-simplify",
-            "transform-simplify-comparison-operators"
+            // "@babel/plugin-transform-minify-booleans",
+            // "minify-builtins",
+            // "minify-dead-code-elimination", { "optimizeRawSize": true },
+            // "minify-mangle-names",
+            // "minify-numeric-literals",
+            // "minify-simplify",
+            // "transform-simplify-comparison-operators",
+            "@babel/plugin-proposal-class-properties", { "loose": true },
         ],
     ]
 };
 
-mix.react('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/loader.js', 'public/js')
+    // .scripts(['resources/js/polyfill.js'], 'public/js/polyfill.js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('public/js/app.js', 'resources/frontend/js/app.js');
+    // .copy('public/js/polyfill.js', 'resources/frontend/js/polyfill.js');
